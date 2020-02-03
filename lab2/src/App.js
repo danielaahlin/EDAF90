adab570338487743cb34bd13b0fe8ee5f7285c88
Daniel Åhlin <daniel.ahlin96@gmail.com>
2020-02-03 10:05:21 +0100

    continue with lab2

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
+import ComposeSaladModal from './ComposeSaladModal';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="jumbotron text-center">
-          <h1>My Own Salad Bar</h1>
-          <p>Here you can order custom made salads!</p> 
-         </div>
-        <div>
-          Add your own react component here!
-      </div>
-      <ComposeSalad inventory={inventory} />
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>My Own Salad Bar</h1>
+                    <p>Here you can order custom made salads!</p>
+                </div>
+                <ul>
+                    <ComposeSaladModal inventory={inventory} />
+                </ul>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -3,16 +3,71 @@ import React, { Component } from 'react';
 class ComposeSalad extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {foundation : '', proteins : [], extras : [], dressing : ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+
+    }
+
+    handleSubmit(event) {
+
+    }
+
     render() {
         const inventory = this.props.inventory;
         let foundations = Object.keys(inventory).filter(name => inventory[name].foundation);
+        let proteins = Object.keys(inventory).filter(name => inventory[name].protein);
+        let extras = Object.keys(inventory).filter(name => inventory[name].extra);
+        let dressings = Object.keys(inventory).filter(name => inventory[name].dressing);
         return (
             <div className="container">
-                <ul>
-                    {foundations.map(name => <li key={name}>{name}</li>)}
-                </ul>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Choose foundation:
+                        <select onChange={this.handleChange}>
+                            {foundations.map(name => <option value={name}>{name}</option>)}
+                        </select>
+                    </label>
+
+                    <div>
+                        <label>
+                            Choose protein:
+                            {proteins.map(name =>
+                                <div>
+                                    <input type="checkbox" id={name}></input>
+                                    <label>{name}</label>
+                                </div>
+                            )}
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Choose extras:
+                            {extras.map(name =>
+                                <div>
+                                    <input type="checkbox" id={name}></input>
+                                    <label>{name}</label>
+                                </div>
+                            )}
+                        </label>
+                    </div>
+
+                    <label>
+                        Choose dressing:
+                        <select onChange={this.handleChange}>
+                            {dressings.map(name => <option value={name}>{name}</option>)}
+                        </select>
+                    </label>
+
+
+
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         );
     }
